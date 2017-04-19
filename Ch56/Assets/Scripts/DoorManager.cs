20e37ca7b8044cff1b931c57a6f823743e129962
Andriy Yavorskyy <yavoan@uga.edu>
2017-04-19 08:28:42 -0400

    unity

---
+++ Ch56/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour {
+
+	bool doorIsOpen = false;
+	float doorTimer = 0.0f;
+	public float doorOpenTime = 3.0f;
+	public AudioClip doorOpenSound;
+	public AudioClip doorShutSound;
+	void Start(){
+		doorTimer = 0.0f;
+	}
+	void Update(){
+		if(doorIsOpen){
+			doorTimer += Time.deltaTime;
+			if(doorTimer > doorOpenTime){
+				Door(doorShutSound, false, "doorshut");
+				doorTimer = 0.0f;
+			}
+		}
+	}
+	void DoorCheck(){
+		if(!doorIsOpen){
+			Door(doorOpenSound, true, "dooropen");
+		}
+	}
+	void Door(AudioClip aClip, bool openCheck, string animName){
+		GetComponent<AudioSource>().PlayOneShot(aClip);
+		doorIsOpen = openCheck;
+		transform.parent.gameObject.GetComponent<Animation>().Play(animName);
+	}
+}
---
+++ Ch56/Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour {
+	GameObject currentDoor;
+	void Update () {
+		RaycastHit hit;
+		if(Physics.Raycast (transform.position, transform.forward,
+			out hit, 3)) {
+			if(hit.collider.gameObject.tag=="playerDoor"){
+				currentDoor = hit.collider.gameObject;
+				currentDoor.SendMessage("DoorCheck");
+			}
+		}
+	}
+}
